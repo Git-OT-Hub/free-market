5a739dbf4ac2570e62ea48cfc3c07bf096167d37
Git-OT-Hub <nw7649sss@gmail.com>
2025-09-29 23:05:25 +0900

    add: チャット機能に関するテーブル作成

---
+++ backend/database/migrations/2025_09_29_224629_create_chat_reads_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chat_reads', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('chat_id')
+                ->constrained('chats')
+                ->onDelete('cascade');
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->onDelete('cascade');
+            $table->timestamp('read_at')->nullable();
+            $table->timestamps();
+
+            $table->unique(['chat_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chat_reads');
+    }
+};
